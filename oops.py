28cc83a1fa77824c9901a516ea3662d6059824cd
satendra <yadavsaten9834@gmail.com>
2024-10-15 19:52:31 +0530

    Core python completed

--- oops.py
+++ oops.py
@@ -47,10 +47,13 @@
 
 
 
-#-------------------class members--------------------
+#-------------------@staticmethod & @classmethod--------------------
 
-# class member will be created even without creating object.
-# To create the statice member function we have to use the decorator i.e @staticmethod 
+# class / static  member will be created even without creating object.
+# To create the statice member function we have to use the decorator i.e @staticmethod, and for class @classmethod
+# @staticmethod does not access the member of the class. it is used to do the static task only.
+# @classmethod will access the members of the class and perform the changes in the members accordinly.
+# instance method access the members of the instance and have self in it.
 # Decorator is a function which take the function as an argument and convert it into static member function and return the function.
 # To call the class members we can use classname.name/classname.show() or normally with object name.
 
@@ -72,24 +75,93 @@
 # print(s1.college)
 
 
-class Details:
-    list = []
-    def __init__(self, name, list):
-        self.name = name
-        self.list.append(list[0])
-        self.list.append(list[1])
-        self.list.append(list[2])
+# class Details:
+#     list = []
+#     def __init__(self, name, list):
+#         self.name = name
+#         self.list.append(list[0])
+#         self.list.append(list[1])
+#         self.list.append(list[2])
     
-    @staticmethod   #decorator
+#     @staticmethod   #decorator
     
-    def welcome():
-        print("Hello student, welcome to NITK")
+#     def welcome():
+#         print("Hello student, welcome to NITK")
     
-    def avg(self):
-        print("Average marks of ",self.name)
-        return (self.list[0] + self.list[1] + self.list[2]) / 3
+#     def avg(self):
+#         print("Average marks of ",self.name)
+#         return (self.list[0] + self.list[1] + self.list[2]) / 3
 
-marks = [40,49,40]
-d = Details("satendra",marks)
-Details.welcome()
-print(" is ", d.avg())
+# marks = [40,49,40]
+# d = Details("satendra",marks)
+# Details.welcome()
+# print(" is ", d.avg())
+
+
+# class Student:
+#     name = "satendra"
+
+#     @staticmethod
+#     def show():
+#         print("hello")
+
+
+#     @classmethod
+#     def change(cls):
+#         print(cls)
+#         cls.name = "jatin"
+#         print(Student.name)
+
+# s = Student()
+# Student.change()
+    
+
+
+#-----------------access specifier and delete ----------------
+
+# del objname -> it will delete the object
+# By default all the member function and member varibale of the class are public.
+# To make them private we use __function or __variable.that is we use underscor just before the variable name or function name
+
+# class Display:
+#     __name = "satendra"
+#     def __init__(self):
+#         pass
+    
+#     def __show(self):
+#         print("This is the private method")
+    
+#     def callme(self):
+#         self.__show()
+#         print("private method call by using the public method")
+
+# d = Display()
+# # print(d.name)
+# del d
+# d.callme()
+
+
+#---------------- @property  decorator ----------------
+
+# when we want to define the function as a property(variable) then we will use the property decorator.
+# Suppose if we defind any property inside the constructor then as we know construtor will run only once. but if any changes made in the
+# values then it will not change the property value.
+# This can solve by using the function also. but instead of that we can use the @property decorator.
+
+
+class Student:
+
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
+        
+    
+    @property
+    def per(self):
+        return str((self.a + self.b + self.c) / 3)
+
+obj = Student(89,89,89)
+print(obj.per)              # function as a varibale or attribute
+obj.c = 100
+print(obj.per)
--- oops_pillar.py
+++ oops_pillar.py
@@ -22,34 +22,187 @@
 # print(s.showDetails())
 # Student.show()
 
+
+
+
+
+
+
+
 #---------------- Encapsulation------------
 
 # Wrapping all the information related to single entity inside the one class is called as  encapsultion.
 # In the above example student is the entity and we have wrap all the method and variable related to the student in the student class.
 
-class Account:
+# class Account:
 
-    def __init__(self, balance):
-        self.balance = balance
+#     def __init__(self, balance):
+#         self.balance = balance
     
-    @staticmethod
+#     @staticmethod
 
-    def notes():
-        print("Central Bank of India", end =" ")
+#     def notes():
+#         print("Central Bank of India", end =" ")
 
-    def credit(self, amount):
-        self.notes()
-        print("Account Credited with Rs.", self.balance)
-        self.balance += amount
-        print("Total balance Rs.", self.balance)
+#     def credit(self, amount):
+#         self.notes()
+#         print("Account Credited with Rs.", self.balance)
+#         self.balance += amount
+#         print("Total balance Rs.", self.balance)
 
-    def debited(self, amount):
-        self.notes()
-        print("Account Debited with Rs.", self.balance)
-        self.balance -= amount
-        print("Total balance Rs.", self.balance)
+#     def debited(self, amount):
+#         self.notes()
+#         print("Account Debited with Rs.", self.balance)
+#         self.balance -= amount
+#         print("Total balance Rs.", self.balance)
 
 
-acc = Account(10000)
-acc.credit(9000)
-acc.debited(1000)
+# acc = Account(10000)
+# acc.credit(9000)
+# acc.debited(1000)
+
+
+
+#--------------------inheritance---------------
+
+
+# Three types of inheritance in python singleLevel, multiLevel, and multiple inheritance.
+# To inherit parent class into child class we use () breaces.
+# super() key word is used to call the constructor or method of the parent class inside the child class.
+# order of execution of the constructor is depend on the order of calling.
+
+
+# single level
+
+
+# class A:
+#     varA = "A class"
+#     def __init__(self, name):
+#         print("You are inside the parent constructor")
+#         self.name = name
+    
+#     def display(self):
+#         print("The name of class is", self.name)
+
+# class B(A):
+#     varB = "B class"
+
+#     def __init__(self, name1, name2):
+#         super().__init__(name1) 
+#         print("You are inside the child constructor")             
+#         self.name2 = name2
+        
+    
+#     def show(self):
+#         print("The Name of class is ",self.name2)
+
+# child = B("Parent", "child")
+# child.display()
+
+
+
+
+# multilevel 
+
+
+
+# class A:
+#     varA = "A class"
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def display(self):
+#         print("The name of class is", self.name)
+
+# class B(A):
+#     varB = "B class"
+
+#     def __init__(self, name1, name2):
+#         super().__init__(name1)             
+#         self.name2 = name2
+        
+#     def show(self):
+#         print("The Name of class is ",self.name2)
+
+
+# class C(B):
+    
+#     def __init__(self, name1, name2, name3):
+#         super().__init__(name1, name2)
+#         self.name3 = name3
+    
+#     def show1(self):
+#         print("This is grandchild", self.name3)
+
+
+# child = C("grandparent", "parent", "child")
+# child.show1()
+
+
+
+# multiple 
+
+
+# class A:
+#     varA = "A class"
+#     def __init__(self, name):
+#         self.name = name
+    
+#     def display(self):
+#         print("The name of class is", self.name)
+
+# class B:
+#     varB = "B class"
+
+#     def __init__(self, name2):     
+#         self.name2 = name2
+        
+#     def show(self):
+#         print("The Name of class is ",self.name2)
+
+
+# class C(A, B):
+    
+#     def __init__(self, name1, name2, name3):
+#         A.__init__(self, name1)
+#         B.__init__(self, name2)
+#         self.name3 = name3
+    
+#     def show1(self):
+#         print("The name of the class is ", self.name3)
+
+
+# child = C("grandparent", "parent", "child")
+# child.display()
+# child.show()
+# child.show1()
+
+
+
+
+# ---------------- polymorphisem -----------------
+
+# poly means many and morphisem mean forms.
+# when same things(function/operator) have the different working then it is called as the polymorphisem
+# In python polymorhisem is acheive through operator overloadning.
+# Operator overloading is acheive with the help of the dunder function.
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+    
+    def show(self):
+        print(self.real,"+",self.img,"i")
+    
+    def __add__(self, num2):                # dunder function
+        self.newreal = self.real + num2.real
+        self.newimg = self.img + num2.img
+        return Complex(self.newreal , self.newimg)
+    
+num1 = Complex(3, 5)
+num1.show()
+num2 = Complex(2, 4)
+num2.show()
+num3 = num1 + num2
+num3.show()
